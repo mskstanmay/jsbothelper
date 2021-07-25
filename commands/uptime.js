16aa918b68e13f4479956395edf6e3ba03ada6f6
Msks TanmaY <mskstanmay@gmail.com>
2021-07-25 15:04:16 +0530

    desc change

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,7 +2,7 @@ const ms = require("ms");
 module.exports = {
   slash: "both",
   testOnly: true,
-  description: "Gives The Bot Ws Ping",
+  description: "Gives The Bot's Uptime",
   callback: ({ client }) => {
     return ms(client.uptime);
   },
